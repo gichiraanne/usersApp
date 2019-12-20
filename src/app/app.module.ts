dd157e665335741eaa550c0e65780dbcace65d50
Annie <gichiraanne@gmail.com>
2019-12-20 15:19:24 +0300

    Add httpClientModule and InmemoryWebApiModule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataApi  } from './data-api';
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './users/user-list.component';
 import { DataService } from './data.service';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +15,9 @@ import { DataService } from './data.service';
     UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(DataApi)
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
