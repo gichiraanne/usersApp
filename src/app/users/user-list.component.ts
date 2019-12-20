b37f8018d327613df9629aafdc3b23ffe1f4681f
Annie <gichiraanne@gmail.com>
2019-12-20 11:14:51 +0300

    Add a html template to display users

--- src/app/users/user-list.component.ts
+++ src/app/users/user-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-user-list',
-  template: '<h1>User list component</h1>',
+  templateUrl: 'user-list.component.html',
   styles: []
 })
 
