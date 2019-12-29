3169fe1b20ec44bf412aee6f5bf5d553d1e54a2d
Annie <gichiraanne@gmail.com>
2019-12-29 12:05:40 +0300

    Add userComponent to the app router

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
 
 export const appRoutes: Routes = [
   {path:'users', component: UsersComponent },
+  {path:'user', component: UserComponent },
   {path:'', redirectTo:'/users', pathMatch:'full' }
 ]
 
