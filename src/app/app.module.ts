cab470ca9762a85e873530cd98ebb481f25278bb
Annie <gichiraanne@gmail.com>
2019-12-29 11:40:32 +0300

    Use the added router in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { DataApi  } from './data-api';
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './users/user-list.component';
 import { DataService } from './data.service';
+import { appRoutes } from './app.router';
 
 
 @NgModule({
@@ -17,6 +19,7 @@ import { DataService } from './data.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes),
     InMemoryWebApiModule.forRoot(DataApi)
   ],
   providers: [DataService],
