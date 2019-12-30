9bdd1f7e96342c7f1d7db7cf5c091cff201537f0
Annie <gichiraanne@gmail.com>
2019-12-30 10:46:52 +0300

    Use the user form in the UserNewComponent

--- src/app/user/user-new.component.ts
+++ src/app/user/user-new.component.ts
@@ -2,10 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-user-new',
-  template: `
-  <h1>Add new user</h1>
-  <button class="btn btn-sm btn-primary mr-3 mt-3" type="button" routerLink="/users">All users</button>
-  `
+  templateUrl: 'user-form.component.html'
 })
 
 export class UserNewComponent{
