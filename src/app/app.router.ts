cd437f7939196e7e07b9f11a1b65b89111f5a45d
Annie <gichiraanne@gmail.com>
2019-12-31 06:55:59 +0300

    Add userEditComponent to app router

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,10 +2,12 @@ import { Routes } from "@angular/router";
 import { UsersComponent } from './users/users.component';
 import { UserViewComponent } from './user/user-view.component';
 import {  UserNewComponent } from './user/user-new.component';
+import { UserEditComponent } from './user/user-edit.component';
 
 export const appRoutes: Routes = [
   {path:'users', component: UsersComponent },
   {path:'users/new', component: UserNewComponent },
+  {path:'users/edit', component: UserEditComponent },
   {path:'users/:id', component: UserViewComponent },
   {path:'', redirectTo:'/users', pathMatch:'full' }
 ]
