c0d14978c4d025f0a16a36a91d90cea855cbb4e6
Annie <gichiraanne@gmail.com>
2019-12-31 07:05:17 +0300

    Use getUser function to updateUser details

--- src/app/user/user-edit.component.ts
+++ src/app/user/user-edit.component.ts
@@ -1,10 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../users/user';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-user-edit',
-  template: "<h1>Update user</h1>"
+  template: "<h1>Update {{updateUser?.name}}</h1>"
 })
 
-export class UserEditComponent{
+export class UserEditComponent implements OnInit {
+  updateUser: User;
+  updateUserID: Number;
 
+  constructor(private activatedRoute:ActivatedRoute, private router:Router, private dataService:DataService){}
+
+  ngOnInit(){
+    this.updateUserID = this.activatedRoute.snapshot.params['id'];
+
+    this.dataService.getUser(this.updateUserID).subscribe(data =>{
+      this.updateUser = data
+    })
+
+  }
 }
