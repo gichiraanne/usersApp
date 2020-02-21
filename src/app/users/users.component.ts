734a937221cf0db2ab0d4b9a98e6e3e3b245bfba
Annie <gichiraanne@gmail.com>
2020-02-21 20:47:06 +0300

    Adjust user-list to use state

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { User } from './user';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'app-users',
@@ -7,31 +10,32 @@ import { DataService } from '../data.service';
   styleUrls: ['users.component.scss']
 })
 
-export class UsersComponent implements OnInit {
-  users;
-  user;
+export class UsersComponent {
+  users: Observable<User[]>;
 
-  constructor(private dataService: DataService) { }
-
-  ngOnInit() {
-    this.showusers()
+  constructor(private store: Store<{ users: User[] }>) {
+    this.users = this.store.pipe(select('users'));
   }
 
+  // ngOnInit() {
+  //   // this.showusers()
+  // }
 
-  showusers() {
-    this.dataService.getUsers().subscribe(data => {
-      this.users = data;
-    })
-  }
 
-  deleteUser(id, name) {
-    if (confirm(`Are you sure you want to delete ${name}?`)) {
-      this.dataService.deleteUser(id).subscribe(data => {
-        this.showusers()
-      })
-    }
+  // showusers() {
+  //   this.dataService.getUsers().subscribe(data => {
+  //     this.users = data;
+  //   })
+  // }
 
-  }
+  // deleteUser(id, name) {
+  //   if (confirm(`Are you sure you want to delete ${name}?`)) {
+  //     this.dataService.deleteUser(id).subscribe(data => {
+  //       this.showusers()
+  //     })
+  //   }
+
+  //}
 
 
 }
