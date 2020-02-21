c91bd65c1ef0dabec5fb37eee049fd4e5d7abe9c
Annie <gichiraanne@gmail.com>
2020-02-21 20:46:01 +0300

    Reducers file for users

---
+++ src/app/users/user.reducers.ts
@@ -0,0 +1,16 @@
+
+import {ActionEx, CustomerActionTypes} from './user.actions';
+export const initialState = [];
+export function UserReducer(state = initialState, action: ActionEx) {
+  switch (action.type) {
+    case CustomerActionTypes.Add:
+      return [...state, action.payload];
+    case CustomerActionTypes.Remove:
+      return [
+        ...state.slice(0, action.payload),
+        ...state.slice(action.payload + 1)
+      ];
+    default:
+      return state;
+  }
+}
