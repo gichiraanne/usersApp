a4cd1deaa8a1676e20262f93a222ef5ea3363b5d
Annie <gichiraanne@gmail.com>
2020-02-22 17:48:16 +0300

    Update actions and reducers

--- src/app/users/user.actions.ts
+++ src/app/users/user.actions.ts
@@ -1,17 +1,30 @@
 import {Action} from '@ngrx/store';
 export enum CustomerActionTypes {
-  Add = '[Customer Component] Add',
-  Remove = '[Customer Component] Remove'
+  LoadUsers = '[Users Component] load users from server',
+  LoadSuccess = '[Users Component] load success',
+  Add = '[AddUser Component] Add user',
+  Remove = '[Users Component] Remove user'
 }
 export class ActionEx implements Action {
   readonly type;
   payload: any;
 }
+export class UsersLoad implements Action {
+  readonly type = CustomerActionTypes.LoadUsers;
+}
+
+export class LoadUsers implements ActionEx {
+  readonly type = CustomerActionTypes.LoadSuccess;
+  constructor(public payload: any) {
+  }
+}
+
 export class CustomerAdd implements ActionEx {
   readonly type = CustomerActionTypes.Add;
   constructor(public payload: any) {
   }
 }
+
 export class CustomerRemove implements ActionEx {
   readonly type = CustomerActionTypes.Remove;
   constructor(public payload: any) {
--- src/app/users/user.reducers.ts
+++ src/app/users/user.reducers.ts
@@ -3,8 +3,12 @@ import {ActionEx, CustomerActionTypes} from './user.actions';
 export const initialState = [];
 export function UserReducer(state = initialState, action: ActionEx) {
   switch (action.type) {
+    case CustomerActionTypes.LoadSuccess:
+      return [...state, ...action.payload];
+
     case CustomerActionTypes.Add:
       return [...state, action.payload];
+
     case CustomerActionTypes.Remove:
       return [
         ...state.slice(0, action.payload),
